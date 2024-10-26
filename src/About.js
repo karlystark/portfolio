4ff45f9887749253017b7242143a6ed3a78f54e8
Karly Stark <karlystark@gmail.com>
2024-10-25 19:16:58 -0700

    adding link for tlm

--- src/About.js
+++ src/About.js
@@ -25,7 +25,7 @@ function About() {
         I'm a full-stack software engineer with a passion for accessibility and
         making things that bring people closer to their communities. I currently
         spend my days teaching full-stack web development (MERN stack) and building
-        projects with folks at The Last Mile.
+        projects with folks at <a href="https://thelastmile.org/">The Last Mile</a>.
         </p>
         <QuestionSelect />
       </div>
